7340f455b25faefb17b0f1af143934cc6048faed
Dheer Guda <gudadheer@gmail.com>
2024-08-02 03:22:15 -0400

    updates settings, works with first 5 letters

--- ASL_translator.py
+++ ASL_translator.py
@@ -4,8 +4,6 @@ import mediapipe as mp
 import numpy as np
 import warnings
 
-
-
 warnings.filterwarnings("ignore", category=UserWarning, module="google.protobuf")
 model_dict = pickle.load(open('./model.p', 'rb'))
 model = model_dict['model']
@@ -18,9 +16,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J',
-               10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'V', 18: 'S', 19: 'T',
-               20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z', 26: 'Space', 27: 'Back' }
+labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E'}
 
 while True:
     ret, frame = cap.read()
@@ -38,27 +34,28 @@ while True:
 
     results = hands.process(frame_rgb)
     if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                frame,  # image to draw
-                hand_landmarks,  # model output
-                mp_hands.HAND_CONNECTIONS,  # hand connections
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style())
+        # Only process the first hand detected
+        hand_landmarks = results.multi_hand_landmarks[0]
+        
+        mp_drawing.draw_landmarks(
+            frame,  # image to draw
+            hand_landmarks,  # model output
+            mp_hands.HAND_CONNECTIONS,  # hand connections
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
 
-        for hand_landmarks in results.multi_hand_landmarks:
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
+        for i in range(len(hand_landmarks.landmark)):
+            x = hand_landmarks.landmark[i].x
+            y = hand_landmarks.landmark[i].y
 
-                x_.append(x)
-                y_.append(y)
+            x_.append(x)
+            y_.append(y)
 
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
-                data_aux.append(x - min(x_))
-                data_aux.append(y - min(y_))
+        for i in range(len(hand_landmarks.landmark)):
+            x = hand_landmarks.landmark[i].x
+            y = hand_landmarks.landmark[i].y
+            data_aux.append(x - min(x_))
+            data_aux.append(y - min(y_))
 
         x1 = int(min(x_) * W) - 10
         y1 = int(min(y_) * H) - 10
